e16c53ea4a5a05472f85b9d88f8f4d07ee4ea21c
P1ST-0N <luckylionia@gmail.com>
2023-06-21 21:15:23 +0300

    fixed path filter

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,13 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
 
-import contactsReducer from './contacts/contacts-slice';
-import filterReducer from "components/Filter/filter-reducer";
+import contactsReducer from "./contacts/contacts-slice";
+import filterReducer from "./filter/filter-reducer";
 
 export const store = configureStore({
-    reducer: {
-        contacts: contactsReducer,
-        filter: filterReducer,
-    },
+  reducer: {
+    contacts: contactsReducer,
+    filter: filterReducer,
+  },
 });
 
 export default store;
